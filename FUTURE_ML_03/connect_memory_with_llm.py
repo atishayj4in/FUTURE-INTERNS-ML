96a89bfcc550bde9401d965705a32031e8c38aa3
Atishay Jain <atishayjain0002@gmail.com>
2025-06-27 10:26:33 +0530

    Create connect_memory_with_llm.py

---
+++ FUTURE_ML_03/connect_memory_with_llm.py
@@ -0,0 +1,47 @@
+import os
+from langchain.prompts import PromptTemplate
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_groq import ChatGroq
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+def load_llm():
+    llm = ChatGroq(
+        model_name="llama3-70b-8192",
+        temperature=0,
+        api_key=""  #Enter Your Groq API key here
+    )
+    return llm
+
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context
+
+Context: {context}
+Question: {question}
+if someone says hello or hi or greet you, respond them with appropriate greetings
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+DB_FAISS_PATH = "vectorstore/db_faiss"
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=load_llm(),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k': 3}),
+    return_source_documents=True,
+    chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+
+user_query = input("Write Query Here: ")
+response = qa_chain.invoke({'query': user_query})
+print("RESULT: ", response["result"])
